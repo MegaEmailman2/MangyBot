068fd45e45ff9a55d91670e74d7de05b21cb9987
MegaEmailman2 <hunterreedm@icloud.com>
2024-11-15 21:33:50 -0500

    Several fixes, mostly typos and timing.

--- Main.py
+++ Main.py
@@ -3,43 +3,47 @@
 import asyncio
 import os
 import discord
+import subprocess
 import psutil
 from discord.ext.commands import Bot
 
 intents = discord.Intents.default()
-Intents.message_content = True
+intents.message_content = True
 
 with open("TokenFile.txt", encoding="utf-8") as TokenFile:
         token=TokenFile.read()
 
-client = Bot(command_prefix="!Server", intents=intents)
+client = Bot(command_prefix="!Server ", intents=intents)
 
 @client.command(name='start',
                 description="Starts a server decided by SE, TC, or (Future Project)",
-                brief="Starts a server."
-                usage="'!Server start TC' will boot The Center"
+                brief="Starts a server.",
+                usage="'!Server start TC' will boot The Center",
                 pass_context=True)
 async def start(context):
     outstring = "Attempting to start the server now."
-    target = context.message_content[13:14]
+    target = context.message.content[14:]
     if target == "TC":
-        os.system(PATH/TO/TC/BAT)   #Fill in
+        subprocess.Popen("C:\\Users\\Backup\\Desktop\\Center.bat", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT) 
     elif target == "SE":
-        os.system(PATH/TO/SE/BAT)   #Fill in
+        subprocess.Popen("C:\\Users\\Backup\\Desktop\\Scorch.lnk", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     await context.send(outstring)
     
 @client.command(name='stop',
                 description="Stops a server decided by SE, TC, or (Future Project)",
-                brief="Stops a server."
-                usage="'!Server stop TC' will stop The Center"
+                brief="Stops a server.",
+                usage="'!Server stop TC' will stop The Center",
                 pass_context=True)
 async def stop(context):
-    target = context.message_content[13:14]
+    procname=""
+    target = context.message.content[13:]
     if target == "TC":
-        procname = etsuTC   ###Experimental
+        procname = "Center"   ###Experimental
     elif target == "SE":
-        procname = etsuSE   ###Experimental
+        procname = "etsuSE"   ###Experimental
     for process in psutil.process_iter():
+        print(procname)
+        print(process.name())
         if procname in process.name():
             process.kill()
     outstring = ("Attempting to stop the server running " + target + ".")
@@ -47,13 +51,20 @@ async def stop(context):
     
 @client.command(name='update',
                 description="Updates the game servers.  Stop them first.",
-                brief=""
-                usage="!Server update"
+                brief="",
+                usage="!Server update",
                 pass_context=True)
 async def update(context):
     outstring = "Attempting to update the servers.  Please wait 10 minutes before starting them."
-    #Run the update batch file
+    subprocess.Popen("C:\\Users\\Backup\\Desktop\\Update.bat")
     await context.send(outstring)
+
+@client.command(name='test',
+		description="Guess.",
+		usage="!Server test",
+		pass_context=True)
+async def test(context):
+	await context.send("GG The bot is live.")
               
     
 def startup():
